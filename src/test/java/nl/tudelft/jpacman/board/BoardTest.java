f514c204870f8110279517e2ae1348ab8bc35660
Watsapol Samittivate <97512569+MarkMPW@users.noreply.github.com>
2023-09-20 17:35:02 +0700

    boardTest

---
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -0,0 +1,40 @@
+package nl.tudelft.jpacman.board;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+public class BoardTest {
+    private Board board;
+    int width = 1;
+    int height = 1;
+    @BeforeEach
+    void setUp() {
+        Square[][] grid = new Square[width][height];
+        grid[0][0] = new BasicSquare();
+        board = new Board(grid);
+    }
+
+    @Test
+    void testInvariant() {
+        assertThat(board.invariant()).isNotNull();
+    }
+
+    @Test
+    void testWidth() {
+        assertThat(board.getWidth()).isEqualTo(width);
+    }
+
+    @Test
+    void testHeight() {
+        assertThat(board.getHeight()).isEqualTo(height);
+    }
+
+    @Test
+    void testSquareAtWithNullSquare() {
+        Square[][] grid = new Square[width][height];
+        grid[0][0] = null;
+        Board boardWithNull = new Board(grid);
+        Square square = boardWithNull.squareAt(0, 0);
+        assertThat(square).isNull();
+    }
+}
